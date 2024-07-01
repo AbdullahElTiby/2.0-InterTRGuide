a623cf18bf2480718a03907c2cb5fe4ff77fcb38
AbdullahElTiby <abd.eltiby@gmail.com>
2024-07-01 14:55:54 +0300

    Update static file configuration in settings.py and base.html

--- intertrguide/intertrguide/settings.py
+++ intertrguide/intertrguide/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_DIR = BASE_DIR / 'static'
+
+# settings.py
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
