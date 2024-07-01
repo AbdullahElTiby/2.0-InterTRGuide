14bac1114653b9f7aaf192e032b9c1fb1d13e4f7
AbdullahElTiby <abd.eltiby@gmail.com>
2024-07-01 17:27:45 +0300

    feat: Add new pages and views for AI Tour Guide, Pricing, About, and Contact

--- intertrguide/app/urls.py
+++ intertrguide/app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('aitg', views.aitg, name='aitg'),
+    path('pricing', views.pricing, name='pricing'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
 ]
--- intertrguide/app/views.py
+++ intertrguide/app/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+
     return render(request, 'index.html')
+def aitg(request):
+
+    return render(request, 'aitg.html')
+def pricing(request):
+
+    return render(request, 'pricing.html')
+
+def about(request):
+
+    return render(request, 'about.html')
+
+def contact(request):
+
+    return render(request, 'contact.html')
